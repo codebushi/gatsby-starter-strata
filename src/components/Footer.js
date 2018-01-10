bc78891d08182dc32f8ae4af159faecd5fd86802
Hunter Chang <hunter@madwiremedia.com>
2018-01-09 17:15:57 -0700

    new images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
                         <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
                     </ul>
                 </div>
             </footer>
