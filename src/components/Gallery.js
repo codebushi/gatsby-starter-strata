c48d567d570cb54b7a401fa1e72dfdba2aafc8cf
Hunter Chang <hunter@madwiremedia.com>
2018-01-09 18:23:55 -0700

    add image gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,106 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Lightbox from 'react-images';
+
+class Gallery extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            lightboxIsOpen: false,
+            currentImage: 0,
+        };
+
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+        this.gotoImage = this.gotoImage.bind(this);
+        this.handleClickImage = this.handleClickImage.bind(this);
+        this.openLightbox = this.openLightbox.bind(this);
+    }
+    openLightbox (index, event) {
+        event.preventDefault();
+        this.setState({
+            currentImage: index,
+            lightboxIsOpen: true,
+        });
+    }
+    closeLightbox () {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
+    }
+    gotoPrevious () {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
+    }
+    gotoNext () {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
+    }
+    gotoImage (index) {
+        this.setState({
+            currentImage: index,
+        });
+    }
+    handleClickImage () {
+        if (this.state.currentImage === this.props.images.length - 1) return;
+
+        this.gotoNext();
+    }
+    renderGallery () {
+        const { images } = this.props;
+
+        if (!images) return;
+
+        const gallery = images.map((obj, i) => {
+            return (
+                <article className="6u 12u$(xsmall) work-item" key={i}>
+                    <a
+                        className="image fit thumb"
+                        href={obj.src}
+                        onClick={(e) => this.openLightbox(i, e)}
+                    >
+                        <img src={obj.thumbnail} />
+                    </a>
+
+                    <h3>{obj.caption}</h3>
+                    <p>{obj.description}</p>
+                </article>
+            );
+        });
+
+        return (
+            <div className="row">
+                {gallery}
+            </div>
+        );
+    }
+    render () {
+        return (
+            <div>
+                {this.renderGallery()}
+                <Lightbox
+                    currentImage={this.state.currentImage}
+                    images={this.props.images}
+                    isOpen={this.state.lightboxIsOpen}
+                    onClickImage={this.handleClickImage}
+                    onClickNext={this.gotoNext}
+                    onClickPrev={this.gotoPrevious}
+                    onClickThumbnail={this.gotoImage}
+                    onClose={this.closeLightbox}
+                />
+            </div>
+        );
+    }
+}
+
+Gallery.displayName = 'Gallery';
+Gallery.propTypes = {
+    images: PropTypes.array
+};
+
+export default Gallery;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
+// import Lightbox from 'react-images'
+import Gallery from '../components/Gallery'
+
 import thumb01 from '../assets/images/thumbs/01.jpg'
 import thumb02 from '../assets/images/thumbs/02.jpg'
 import thumb03 from '../assets/images/thumbs/03.jpg'
@@ -10,112 +13,149 @@ import thumb04 from '../assets/images/thumbs/04.jpg'
 import thumb05 from '../assets/images/thumbs/05.jpg'
 import thumb06 from '../assets/images/thumbs/06.jpg'
 
+import full01 from '../assets/images/fulls/01.jpg'
+import full02 from '../assets/images/fulls/02.jpg'
+import full03 from '../assets/images/fulls/03.jpg'
+import full04 from '../assets/images/fulls/04.jpg'
+import full05 from '../assets/images/fulls/05.jpg'
+import full06 from '../assets/images/fulls/06.jpg'
+
+const DEFAULT_IMAGES = [
+    { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+];
+
 class HomeIndex extends React.Component {
-  render() {
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const siteDescription = this.props.data.site.siteMetadata.description
 
-    return (
-        <div>
-            <Helmet>
-                    <title>{siteTitle}</title>
-                    <meta name="description" content={siteDescription} />
-            </Helmet>
+    constructor() {
+        super();
 
-            <div id="main">
+        this.state = {
+            lightboxIsOpen: false,
+            currentImage: 0,
+        };
 
-                <section id="one">
-                    <header className="major">
-                        <h2>Ipsum lorem dolor aliquam ante commodo<br />
-                        magna sed accumsan arcu neque.</h2>
-                    </header>
-                    <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
-                    <ul className="actions">
-                        <li><a href="#" className="button">Learn More</a></li>
-                    </ul>
-                </section>
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+        this.openLightbox = this.openLightbox.bind(this);
+        this.handleClickImage = this.handleClickImage.bind(this);
+    }
 
-                <section id="two">
-                    <h2>Recent Work</h2>
-                    <div className="row">
-                        <article className="6u 12u$(xsmall) work-item">
-                            <a href="images/fulls/01.jpg" className="image fit thumb"><img src={thumb01} alt="" /></a>
-                            <h3>Magna sed consequat tempus</h3>
-                            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
-                        </article>
-                        <article className="6u 12u$(xsmall) work-item">
-                            <a href="images/fulls/02.jpg" className="image fit thumb"><img src={thumb02} alt="" /></a>
-                            <h3>Ultricies lacinia interdum</h3>
-                            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
-                        </article>
-                        <article className="6u 12u$(xsmall) work-item">
-                            <a href="images/fulls/03.jpg" className="image fit thumb"><img src={thumb03} alt="" /></a>
-                            <h3>Tortor metus commodo</h3>
-                            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
-                        </article>
-                        <article className="6u 12u$(xsmall) work-item">
-                            <a href="images/fulls/04.jpg" className="image fit thumb"><img src={thumb04} alt="" /></a>
-                            <h3>Quam neque phasellus</h3>
-                            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
-                        </article>
-                        <article className="6u 12u$(xsmall) work-item">
-                            <a href="images/fulls/05.jpg" className="image fit thumb"><img src={thumb05} alt="" /></a>
-                            <h3>Nunc enim commodo aliquet</h3>
-                            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
-                        </article>
-                        <article className="6u 12u$(xsmall) work-item">
-                            <a href="images/fulls/06.jpg" className="image fit thumb"><img src={thumb06} alt="" /></a>
-                            <h3>Risus ornare lacinia</h3>
-                            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
-                        </article>
-                    </div>
-                    <ul className="actions">
-                        <li><a href="#" className="button">Full Portfolio</a></li>
-                    </ul>
-                </section>
+    openLightbox (index, event) {
+        event.preventDefault();
+        this.setState({
+            currentImage: index,
+            lightboxIsOpen: true,
+        });
+    }
+    closeLightbox () {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
+    }
+    gotoPrevious () {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
+    }
+    gotoNext () {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
+    }
+    handleClickImage () {
+        if (this.state.currentImage === this.props.images.length - 1) return;
 
-                <section id="three">
-                    <h2>Get In Touch</h2>
-                    <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
-                    <div className="row">
-                        <div className="8u 12u$(small)">
-                            <form method="post" action="#">
-                                <div className="row uniform 50%">
-                                    <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                    <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                    <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                </div>
-                            </form>
-                            <ul className="actions">
-                                <li><input type="submit" value="Send Message" /></li>
-                            </ul>
+        this.gotoNext();
+    }
+
+    render() {
+        const siteTitle = this.props.data.site.siteMetadata.title
+        const siteDescription = this.props.data.site.siteMetadata.description
+
+        return (
+            <div>
+                <Helmet>
+                        <title>{siteTitle}</title>
+                        <meta name="description" content={siteDescription} />
+                </Helmet>
+
+                <div id="main">
+
+                    <section id="one">
+                        <header className="major">
+                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
+                            magna sed accumsan arcu neque.</h2>
+                        </header>
+                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+                        <ul className="actions">
+                            <li><a href="#" className="button">Learn More</a></li>
+                        </ul>
+                    </section>
+
+                    <section id="two">
+                        <h2>Recent Work</h2>
+
+                        <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
+                            src,
+                            thumbnail,
+                            caption,
+                            description
+                        }))} />
+
+                        <ul className="actions">
+                            <li><a href="#" className="button">Full Portfolio</a></li>
+                        </ul>
+                    </section>
+
+                    <section id="three">
+                        <h2>Get In Touch</h2>
+                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+                        <div className="row">
+                            <div className="8u 12u$(small)">
+                                <form method="post" action="#">
+                                    <div className="row uniform 50%">
+                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                                    </div>
+                                </form>
+                                <ul className="actions">
+                                    <li><input type="submit" value="Send Message" /></li>
+                                </ul>
+                            </div>
+                            <div className="4u 12u$(small)">
+                                <ul className="labeled-icons">
+                                    <li>
+                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
+                                        1234 Somewhere Rd.<br />
+                                        Nashville, TN 00000<br />
+                                        United States
+                                    </li>
+                                    <li>
+                                        <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
+                                        000-000-0000
+                                    </li>
+                                    <li>
+                                        <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
+                                        <a href="#">hello@untitled.tld</a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="4u 12u$(small)">
-                            <ul className="labeled-icons">
-                                <li>
-                                    <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                    1234 Somewhere Rd.<br />
-                                    Nashville, TN 00000<br />
-                                    United States
-                                </li>
-                                <li>
-                                    <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                    000-000-0000
-                                </li>
-                                <li>
-                                    <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                    <a href="#">hello@untitled.tld</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
+                    </section>
+
+                </div>
 
             </div>
-
-        </div>
-    )
-  }
+        )
+    }
 }
 
 export default HomeIndex
