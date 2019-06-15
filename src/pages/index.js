83b8e02bdd14a1dafbdebee8efe97553677304f0
Ghassen Rjab <rjab.ghassen@gmail.com>
2019-06-15 21:14:25 +0100

    Remove state and methods of the Gallery component

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,50 +30,6 @@ const DEFAULT_IMAGES = [
 
 class HomeIndex extends React.Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
-        };
-
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
-    }
-
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
-
-        this.gotoNext();
-    }
-
     render() {
         const siteTitle = "Gatsby Starter - Strata"
         const siteDescription = "Site description"
