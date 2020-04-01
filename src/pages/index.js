9289d5141f7d1b339dccfb2814e0c0e593647b47
Matt Canty <matthewcanty@gmail.com>
2020-04-01 17:18:27 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,9 +80,9 @@ class HomeIndex extends React.Component {
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
                                         <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
                                     </div>
-                                  <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                  </ul>
+                                    <ul className="actions">
+                                      <li><input type="submit" value="Send Message" /></li>
+                                    </ul>
                                 </form>
                             </div>
                             <div className="4u 12u$(small)">
