25a21862bb7dbb8e6351ba8cefc49b2fca590936
Arthur Ribeiro Geron <arthur.ribeiro.external@bt.com>
2020-05-02 19:24:16 -0400

    Fix 404 not displaying properly

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div id="main">
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
